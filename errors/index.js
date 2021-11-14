f70494b0aeafb2b0a935607c348f09e592c2f976
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-11-14 19:01:55 +0530

    error handling refined

--- errors/index.js
+++ errors/index.js
@@ -1,9 +1,11 @@
 const CustomAPIError = require("./customError");
 const BadRequestError = require("./badRequest");
 const UnauthorizedError = require("./unauthorized");
+const NotFoundError = require("./notFound");
 
 module.exports = {
   CustomAPIError,
   BadRequestError,
   UnauthorizedError,
+  NotFoundError,
 };
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,12 @@
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandler = (err, req, res, next) => {
-  res.status(500).json({ success: false, message: err.mesage });
+  if (err instanceof CustomAPIError) {
+    res.status(err.statusCode).json({ success: false, message: err.message });
+  }
+  res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ success: false, message: err.mesage });
 };
 
 module.exports = errorHandler;
